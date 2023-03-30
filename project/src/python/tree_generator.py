7eb6d5279df8712f21ddd740b7fde40562968c8e
IcaroM-CdC <icaro.moreira@protonmail.com>
2023-03-29 22:44:55 -0300

    [Icaro] refatorando o script de geração das arvores

--- project/src/python/tree_generator.py
+++ project/src/python/tree_generator.py
@@ -7,34 +7,50 @@ import pandas as pd
 import sys
 import os
 
-# dataset_name = sys.argv[1]
 dataset_name = "Iris"
 
 
-dataset = pd.read_csv("../../assets/datasets/" + dataset_name + ".csv")
-# dataset.head()
+dataset = pd.read_csv("heart.csv")
+
+
+def remove_id(dataset):
+
+    dataset = dataset.rename(columns=lambda name: name.lower())
+    column_names = list(dataset)
+
+    for index in range(len(column_names)):
+        if (column_names[index] == "id"):
+            dataset = dataset.drop(columns = "id", axis=1)
+            break
+
+    return dataset
+
+
+def parse_qualitative_variables(dataset, method=1):
+        
+    dataset = pd.get_dummies(dataset, drop_first = True)
+    return dataset
 
 column_names = list(dataset)
 target_column_name = column_names[len(column_names) - 1]
-dataset.rename(columns={target_column_name: "Target"}, inplace=True)
+dataset.rename(columns={target_column_name: "target"}, inplace=True)
 
-# criando a matriz de características X (dataframe) com os valores originais das variáveis, desconsiderando a última
-# coluna (target)
-X = dataset.drop(["Target"], axis=1)
-# criando o vetor de rótulos (saída esperada do modelo para cada amostra)
-Y = dataset["Target"]
+dataset = remove_id(dataset)
+dataset = parse_qualitative_variables(dataset)
 
-# Split dataset into training set and test set
+
+X = dataset.drop(["target"], axis=1)
+Y = dataset["target"]
+
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3)  # 70% training and 30% test
 
 clf = RandomForestClassifier(n_estimators=100)
 
-# Train the model using the training sets y_pred=clf.predict(X_test)
 clf.fit(X_train, y_train)
 
 y_pred = clf.predict(X_test)
 
-print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+# print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
 
 directory = dataset_name
 parent_dir = "../../assets/trees/"
